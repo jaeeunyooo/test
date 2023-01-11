1c30ac2b79a0b193e3543e7ee7ae161cea903d5e
유재은 <frontiersofme@gmail.com>
2023-01-12 04:36:23 +0900

    pid 파일 생성하도록 수정

--- src/main/java/com/jaeeunyoo/test/TestApplication.java
+++ src/main/java/com/jaeeunyoo/test/TestApplication.java
@@ -2,20 +2,14 @@ package com.jaeeunyoo.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class TestApplication {
 
     public static void main(String[] args) {
-        try {
-            ApplicationContext ctx = SpringApplication.run(TestApplication.class, args);
-            DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
-            dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
-            dispatcherServlet.setDispatchOptionsRequest(true);
-        } catch (Exception e) {
-            System.err.println(e.toString());
-        }
+        SpringApplication springApplication = new SpringApplication(TestApplication.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 }
